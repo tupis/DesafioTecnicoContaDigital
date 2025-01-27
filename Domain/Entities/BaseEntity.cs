b2c0006cd0b9d1d15cd79d2117a0f5ff5a0ebeb1
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-27 12:19:23 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; set; } = null;
+        public DateTime? UpdatedAt { get; set; } = null;
+    }
+}
