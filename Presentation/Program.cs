5e02d82af53d51a82e424d5bc9683effdd4a282f
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-30 15:01:49 -0300

    feat: add tokens to swagger and application add dependencies injection

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,3 +1,13 @@
+using Application.Mapping;
+using Application.Services;
+using Infraestructure.Database.PostgreSQL;
+using Infraestructure.Repositories;
+using Infraestructure.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace Presentation
 {
@@ -7,12 +17,81 @@ namespace Presentation
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var jwtSettings = builder.Configuration.GetSection("JWT");
+
             // Add services to the container.
+            builder.Services.AddDbContext<PostgreSQLDBContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+            builder.Services.AddScoped<PostgreSQLDBContext>();
+
+            builder.Services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+            builder.Services.AddScoped<HashPassword>();
+
+            // Repository
+            builder.Services.AddScoped<AccountRepository>();
+            builder.Services.AddScoped<UserRepository>();
+
+            // Services
+            builder.Services.AddScoped<AccountService>();
+            builder.Services.AddScoped<UserService>();
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!)),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information between parties as a JSON object.",
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+
+            });
+
 
             var app = builder.Build();
 
@@ -26,7 +105,6 @@ namespace Presentation
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
