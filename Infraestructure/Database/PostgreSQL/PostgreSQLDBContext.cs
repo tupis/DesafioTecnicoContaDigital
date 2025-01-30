2557ed6f22eef2bbee638b6e1f5fdebe8d959a75
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-30 15:02:15 -0300

    feat: add model to map in db; add relations btw entities

--- Infraestructure/Database/PostgreSQL/PostgreSQLDBContext.cs
+++ Infraestructure/Database/PostgreSQL/PostgreSQLDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Infraestructure.Database.PostgreSQL
@@ -7,13 +8,21 @@ namespace Infraestructure.Database.PostgreSQL
     {
         public PostgreSQLDBContext(DbContextOptions<PostgreSQLDBContext> options) : base(options) { }
 
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var configuration = new ConfigurationBuilder().AddUserSecrets("2c4c9521-3135-466e-afd3-a5ad8aa5c2d0").Build();
+            var configuration = new ConfigurationBuilder().AddUserSecrets("b29dbef5-4977-4d50-81e8-3a6bf331b84b").Build();
             var connString = configuration.GetConnectionString("DefaultConnection");
             optionsBuilder.UseNpgsql(connString);
             base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasOne(u => u.User).WithOne(a => a.Account).HasForeignKey<Account>(a => a.UserId);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
