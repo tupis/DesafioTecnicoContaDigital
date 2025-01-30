cea39104bc885e9c6e907615afff42b374162910
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-30 15:06:15 -0300

    feat: create entity account

---
+++ Domain/Entities/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Account : BaseEntity
+    {
+        public float Balance { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
