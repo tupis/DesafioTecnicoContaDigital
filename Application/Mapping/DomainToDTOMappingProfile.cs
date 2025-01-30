14e4af827519683a2247dd45c7ce9a038a6eac06
João Tupinambá <joaoh.tupinamba@gmail.com>
2025-01-30 15:06:01 -0300

    feat: create mapper entity to dto or reverse

---
+++ Application/Mapping/DomainToDTOMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿
+using AutoMapper;
+
+using Application.Dto;
+using Domain.Entities;
+using Application.DTO.UserDto;
+
+namespace Application.Mapping
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            //Account
+            CreateMap<Account, CreateAccountDto>().ReverseMap();
+            CreateMap<Account, CreateUserAccountResponse>().ReverseMap();
+
+            //User
+            CreateMap<User, CreateUserDto>().ReverseMap();
+            CreateMap<User, CreateUserDtoResponse>()
+                .ForMember(dest => dest.Account, opt => opt.MapFrom(src => src.Account))
+                .ReverseMap();
+        }
+    }
+}
